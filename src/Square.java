cc31481ca70244a0a83f125723c83bc10502afb6
Ali Rezaei <alirezaeim1380@gmail.com>
2023-11-11 13:54:33 +0330

    feat(square): Implement square logic

--- src/Square.java
+++ src/Square.java
@@ -2,4 +2,23 @@ package src;
 
 public class Square implements Shape {
 
+    private double side;
+
+    Square (double side) {
+        this.side = side;
+    }
+
+    public double getSide() {
+        return this.side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double computeArea() {
+        return this.side * this.side;
+    }
+
 }
