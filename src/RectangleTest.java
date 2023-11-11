8371a8bad5f92dbe14c1a8fc6b5c319e69a916d7
armin <armin.delgosarmaher@huma.com>
2023-11-11 13:09:39 +0330

    Fix test syntax issue

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -10,14 +10,14 @@ class RectangleTest {
     @Test
     @DisplayName("Validate simple area calculation")
     void computeAreaSample1() {
-        Rectangle rectangle = new Rectangle(2, 3)
+        Rectangle rectangle = new Rectangle(2, 3);
         assertEquals(2 * 3, rectangle.computeArea());
     }
 
     @Test
     @DisplayName("Validate simple area calculation 2")
     void computeAreaSample2() {
-        Rectangle rectangle = new Rectangle(1.1, 2.2)
+        Rectangle rectangle = new Rectangle(1.1, 2.2);
         assertEquals(1.1 * 2.2, rectangle.computeArea());
     }
 }
