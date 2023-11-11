f5fa04623ef611e8579b7e0caa00aec1a7793e31
armin <armin.delgosarmaher@huma.com>
2023-11-11 13:05:34 +0330

    Add initial failing tests

---
+++ src/RectangleTest.java
@@ -0,0 +1,23 @@
+package src;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RectangleTest {
+
+    @Test
+    @DisplayName("Validate simple area calculation")
+    void computeAreaSample1() {
+        Rectangle rectangle = new Rectangle(2, 3)
+        assertEquals(2 * 3, rectangle.computeArea());
+    }
+
+    @Test
+    @DisplayName("Validate simple area calculation 2")
+    void computeAreaSample2() {
+        Rectangle rectangle = new Rectangle(1.1, 2.2)
+        assertEquals(1.1 * 2.2, rectangle.computeArea());
+    }
+}
